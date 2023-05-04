a161e43caf9389c503cbbb727279fc71cb984f72
Rakibul Islam <rakib011110@gmail.com>
2023-05-04 22:17:42 +0600

    chef data add

--- index.js
+++ index.js
@@ -11,17 +11,7 @@ const chefs = require("./data/data.json")
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-app.get("/chefs", (req, res) => {
-    res.send(chefs)
-})
 
-
-app.get("/chefs/:id", (req, res) => {
-    const id = req.params.id
-    console.log(id);
-    const selectedId = chefs.find(food => food._id === id)
-    res.send(selectedId)
-})
 
 
 app.listen(port, () => {
